597a06e642f1130cb25a155a4385f7e453f03e2c
nocarryr <matt@nomadic-recording.com>
2016-07-23 15:15:51 -0500

    Add utils for decoding datablocks

--- pyltc/audioutils.py
+++ pyltc/audioutils.py
@@ -91,3 +91,71 @@ class LTCDataBlockSampler(Resampler):
             y_max = int(self.y_max / 2)
             a *= y_max
         return self.resample(a)
+
+class ZeroCrossLocator(object):
+    def __init__(self, **kwargs):
+        self.last_sample = None
+    def detect(self, samples):
+        if self.last_sample is not None:
+            samples = np.concatenate((self.last_sample, samples))
+        self.last_sample = samples[-1:]
+        lo = np.sign(samples)
+        lo[lo==0] = -1
+        return np.where(np.diff(lo))[0]
+
+class LTCDataBlockDecoder(ZeroCrossLocator):
+    def __init__(self, **kwargs):
+        super(LTCDataBlockDecoder, self).__init__(**kwargs)
+        self.datablock_callback = kwargs.get('datablock_callback')
+        self.data_buffer = []
+        self.consec_ones = 0
+        self.current_index = 0
+        self.syncword_index = None
+    def iter_decode(self, samples):
+        transitions = self.detect(samples)
+        diff = np.diff(transitions)
+        dmin = diff[1:].min()
+        minrange = list(range(dmin-2, dmin+3))
+        dmax = diff.max()
+        maxrange = list(range(dmax-2, dmax+3))
+        diff_iter = np.nditer(diff)
+        while True:
+            if diff_iter.finished:
+                break
+            v = diff_iter[0]
+            if v in minrange:
+                diff_iter.iternext()
+                yield True
+            elif v in maxrange:
+                yield False
+            diff_iter.iternext()
+    def decode(self, samples):
+        bfr = self.data_buffer
+        i = self.current_index
+        consec_ones = self.consec_ones
+        syncword_index = self.syncword_index
+        for value in self.iter_decode(samples):
+            if value:
+                consec_ones += 1
+            else:
+                consec_ones = 0
+            bfr.append(value)
+            if consec_ones == 12 and syncword_index is None:
+                syncword_index = i + 2
+            if i == syncword_index:
+                if len(bfr) >= 80:
+                    datablock = bfr[-80:]
+                    self.on_datablock(datablock)
+                    syncword_index = None
+                    i = 0
+                    bfr = []
+                else:
+                    syncword_index = None
+            i += 1
+        self.consec_ones = consec_ones
+        self.current_index = i
+        self.syncword_index = syncword_index
+        self.data_buffer = bfr
+    def on_datablock(self, datablock):
+        if self.datablock_callback is not None:
+            self.datablock_callback(datablock)
---
+++ tests/test_decoder.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+def test_decode():
+    from pyltc.tcgen import AudioGenerator
+    from pyltc.audioutils import LTCDataBlockDecoder
+    generated = []
+    decoded = []
+    def on_datablock(datablock):
+        decoded.append(np.array(datablock))
+    g = AudioGenerator(
+        use_current_time=False,
+        bit_depth=16,
+        frame_format={'rate':29.97, 'drop_frame':True},
+    )
+    decoder = LTCDataBlockDecoder(
+        datablock_callback=on_datablock,
+    )
+    for i in range(20):
+        samples = g.generate_frame()
+        in_data = g.get_data_block_array()
+        generated.append(in_data)
+        decoder.decode(samples)
+        g.incr_frame()
+    assert len(decoded) > 0
+    print('num_generated={}, num_decoded={}'.format(len(generated), len(decoded)))
+    offset = None
+    for x, in_data in enumerate(generated):
+        for y, out_data in enumerate(decoded):
+            if np.array_equal(in_data, out_data):
+                offset = (x, y)
+                break
+        if offset is not None:
+            break
+    print('offset={}'.format(offset))
+    assert offset is not None
+    x, y = offset
+    while True:
+        try:
+            in_data = generated[x]
+            out_data = decoded[y]
+        except IndexError:
+            break
+        #print(x, y)
+        #print(np.nonzero(np.not_equal(in_data, out_data)))
+        assert np.array_equal(in_data, out_data)
+        x += 1
+        y += 1
